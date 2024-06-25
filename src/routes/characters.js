e89b40e102aa85af45a5bf20283d4f01cd2904e1
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2024-06-25 15:58:14 +0100

    Add character POST endpoint

--- src/routes/characters.js
+++ src/routes/characters.js
@@ -6,5 +6,21 @@ router.get("/", async (req, res) => {
   const result = await pool.query("SELECT * from characters");
   res.json(result.rows);
 });
+router.post("/", async (req, res) => {
+  const { name, race_id, height, combat_prof, aliases, occupation } = req.body;
+
+  let result;
+  try {
+    result = await pool.query(
+      "INSERT INTO characters (name, race_id, height, combat_prof, aliases, occupation) values ($1, $2, $3, $4, $5, $6) RETURNING *;",
+      [name, race_id, height, combat_prof, aliases, occupation]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.log(`DB error occurred when creating character:\n${err}`);
+    res.status(500).json({ error: "Not telling" });
+    return;
+  }
+});
 
 export default router;
