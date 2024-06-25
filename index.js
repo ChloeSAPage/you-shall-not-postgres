bb03a23c9e209aab828e574acbcd8f526d181500
sbennett <susannah-bennett@hotmail.com>
2024-06-25 15:38:24 +0100

    Added a router

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-
-import pg from "pg";
-const { Pool } = pg;
-
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
+import characters from "./src/routes/characters.js";
+import races from "./src/routes/races.js";
 
 const port = process.env.PORT ?? 3001;
 
@@ -19,10 +10,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", async (req, res) => {
-  const result = await pool.query("SELECT * from characters");
-  res.json(result.rows);
-});
+const router = express.Router();
+router.use("/characters", characters);
+router.use("/races", races);
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Listening on port: http://localhost:${port}.`);
